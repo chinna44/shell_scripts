fdb1ead264ca434ab9ee758b5b0903a112797d79
chinna44 <61354272+chinna44@users.noreply.github.com>
2022-09-13 11:48:33 +0530

    Update log_cleanup&diskspace.sh

--- log_cleanup&diskspace.sh
+++ log_cleanup&diskspace.sh
@@ -30,8 +30,59 @@ for i in $fs_tocheck ; do
    fi
 done
 ============================================================================================================================
+#ziplog.sh
+#!/bin/bash
+set -x
+FS=$(df -h /apps/opt/hnm | awk '{print $5}' | grep [0-9] | tr -d '\%')
+if [ $FS -gt 80 ];
+then
+    log_files=$(ls /apps/opt/hnm/logs/*.log)
+    for logs in $log_files
+      do
+         tar -czvf $logs.tar.gz $logs
+         if [ $? -eq 0 ];
+         then
+             log_files=$(ls /apps/opt/hnm/logs/*.log)
+             for logs in $log_flies
+                do
+                   >$logs
+                done
+         fi
+      done
+fi
+=======================================================================================================================
+##clean up log files
+echo "log cleanup going to remove these files" > /tmp/log_cleanup.out
+
+echo " " >> /tmp/log_cleanup.txt
+echo " " >> /tmp/log_cleanup.txt
+echo "cleaning up /apps/opt/hnm/log/web_vsm/BACKUP" >> /tmp/log_cleanup.txt
+for i in `find /apps/opt/hnm/log/web_vsm/BACKUP -mtime +2`
+do
+ls -ltr $i >> /tmp/log_cleanup.txt
+rm -f $i >> /tmp/log_cleanup.txt
+done
+
+echo " " >> /tmp/log_cleanup.txt
+echo " " >> /tmp/log_cleanup.txt
+echo "switching to /apps/opt/hnm/logs dir" >> /tmp/log_cleanup.txt
+echo " " > /tmp/remove.out
+for i in `find /apps/opt/hnm/logs -mtime +1`
+do
+if echo "$i" | grep log_file_name && `find $i -mtime +5`; then
+       ls -ltr $i >> /tmp/remove.out
+       continue;
+fi
+rm -rf $i >> /tmp/log_cleanup.txt
+done
 
 
+
+
+
+
+         
+         
 
 
 
